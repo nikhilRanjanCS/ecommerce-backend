92388b61b89896497a2bc3083cdcd37fc7b09062
Nikhil Ranjan <nik.ranjancool@gmail.com>
2024-09-04 19:09:52 +0530

    Initial commit, created AppConfig.java, JwtConstant.java, JwtValidator.java. Set up the SecurityFilterChain in AppConfig.java, the jwt token verification and set authentication in JwtValidator.java

---
+++ src/main/java/com/ecommerce/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ecommerce.config;
+
+public class JwtConstant {
+	public static final String SECRET_KEY = "sg$xhhjln09sx9kk-cjhx6w&(v7@dg#ut4ta7y1_3lt7w+ua*8s87u64yn$ls+cb0mjb7pfk#sfg8p5gd3_*wj-e=_4zi(v)w-3-";
+	public static final String JWT_HEADER = "Authorization";
+}
